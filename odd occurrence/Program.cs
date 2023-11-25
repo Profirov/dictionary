ee65d94de72f55464d84ac7c612a6a56a2a2a98d
user <user@DESKTOP-Q7SRQOE>
2023-11-25 12:04:30 +0200

    Add project files.

---
+++ odd occurrence/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] words = Console.ReadLine().Split(" ");
+
+Dictionary<string, int> wordsCount = new();
+
+foreach (string word in words)
+{
+    string caseIntensitiveWord = word.ToLower();
+    if (wordsCount.ContainsKey(caseIntensitiveWord))
+    {
+        wordsCount[caseIntensitiveWord] += 1;
+    }
+    else 
+    { 
+      wordsCount.Add(caseIntensitiveWord, 1);
+    }
+}
+foreach (KeyValuePair<string, int> pair in wordsCount)
+{
+    if (pair.Value % 2 != 0)
+    {
+        Console.Write($"{pair.Key} ");
+    }
+}
